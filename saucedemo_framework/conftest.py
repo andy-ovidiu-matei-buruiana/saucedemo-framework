bc0523b0b6d6d092289618995b5cc7a3b0afa32a
Andy Ovidiu Matei Buruiana <61559732+andy-ovidiu-matei-buruiana@users.noreply.github.com>
2026-01-12 20:15:34 +0200

    Added support for multiple browsers from CLI. Added CLI option for setting timeout values. Create pytest.ini file to solve python import path issue

--- saucedemo_framework/conftest.py
+++ saucedemo_framework/conftest.py
@@ -1,46 +1,68 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--timeout",
+        action="store",
+        default="10",
+        help="Default explicit wait timeout (seconds)",
+        type=int
+    )
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        choices=["chrome", "firefox", "edge", "remote"])
+
+from flows.login_flow import LoginFlow
+from models.login_user import LogInUserInfo
 from pages.inventory_page import InventoryPage
-from pages.login_page import LoginPage
 from testdata.users import VALID_USERS, PASSWORD
 
-
 @pytest.fixture
-def browser():
-    options = webdriver.ChromeOptions()
+def browser(request: pytest.FixtureRequest):
+    browser_name: str = request.config.getoption("--browser")
 
-    # Disable password manager + credential prompts
-    prefs = {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False,
-    }
-    options.add_experimental_option("prefs", prefs)
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
 
-    # reduce random Chrome UI interruptions
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-save-password-bubble")
-    options.add_argument("--no-default-browser-check")
-    options.add_argument("--no-first-run")
-    # use guest to remove password leak prompts
-    options.add_argument("--guest")
+        # Disable password manager + credential prompts
+        prefs = {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False,
+        }
+        options.add_experimental_option("prefs", prefs)
 
-    driver = webdriver.Chrome(options=options)
+        # reduce random Chrome UI interruptions
+        options.add_argument("--disable-notifications")
+        options.add_argument("--disable-save-password-bubble")
+        options.add_argument("--no-default-browser-check")
+        options.add_argument("--no-first-run")
+        # use guest to remove password leak prompts
+        options.add_argument("--guest")
+
+        driver = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+    elif browser_name == "edge":
+        driver = webdriver.Edge()
+    else:
+        raise ValueError(f"Unsupported browser: {browser_name}")
+
     driver.maximize_window()
     driver.switch_to.window(driver.current_window_handle)
     yield driver
     driver.quit()
 
-@pytest.fixture
-def logged_in_inventory(browser: WebDriver) -> InventoryPage:
-    # create Login Page object
-    login = LoginPage(browser, 10)
-    # open page
-    login.open()
-    # login with valid user
-    inventory_page = login.login(VALID_USERS[0], PASSWORD)
-    # verify login successful
-    inventory_page.is_loaded()
+@pytest.fixture(scope="session")
+def timeout(request: pytest.FixtureRequest) -> int:
+    return request.config.getoption("--timeout")
 
-    return inventory_page
+@pytest.fixture
+def inventory_page(browser: WebDriver, timeout: int) -> InventoryPage:
+    user = LogInUserInfo(VALID_USERS[0], PASSWORD)
+    inventory = LoginFlow.login_successful(browser, timeout, user)
+    inventory.is_loaded()
+    return inventory
